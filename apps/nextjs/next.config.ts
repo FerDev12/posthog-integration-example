be59d26d2dba00b0bde832579dcfa9978606161c
Fernando Muñoz <bloom_491@pm.me>
2025-10-17 12:58:37 -0600

    feat(next): enable error tracking

--- apps/nextjs/next.config.ts
+++ apps/nextjs/next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
+import { withPostHogConfig } from "@posthog/nextjs-config";
 
 const nextConfig: NextConfig = {
   /* config options here */
 };
 
-export default nextConfig;
+export default withPostHogConfig(nextConfig, {
+  personalApiKey: process.env.POSTHOG_API_KEY!, // Personal API Key
+  envId: process.env.POSTHOG_ENV_ID!, // Environment ID
+  host: process.env.NEXT_PUBLIC_POSTHOG_HOST, // (optional), defaults to https://us.posthog.com
+  sourcemaps: {
+    // (optional)
+    enabled: true, // (optional) Enable sourcemaps generation and upload, default to true on production builds
+    project: "Spooky Dev", // (optional) Project name, defaults to repository name
+    deleteAfterUpload: true, // (optional) Delete sourcemaps after upload, defaults to true
+  },
+});
