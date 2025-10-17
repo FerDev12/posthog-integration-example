8948453a84b51adf11f68cd755d8380fe615e30c
Fernando Muñoz <bloom_491@pm.me>
2025-10-17 12:34:06 -0600

    feat(next): create github action workflow

--- apps/nextjs/src/db/index.ts
+++ apps/nextjs/src/db/index.ts
@@ -1,7 +1,10 @@
 import "dotenv/config";
 import { drizzle } from "drizzle-orm/node-postgres";
+import { Pool } from "pg";
 import * as schema from "./schema";
 
-export const db = drizzle(process.env.DATABASE_URL!, {
-  schema: schema,
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
 });
+
+export const db = drizzle({ client: pool, schema: schema });
